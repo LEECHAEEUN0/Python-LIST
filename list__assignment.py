71e8a74ca58e778315389bdd02fceeafc369a5f3
unknown <dlcodms3682@naver.com>
2022-05-16 21:14:19 +0900

    파이썬 리스트 과제

---
+++ list__assignment.py
@@ -0,0 +1,15 @@
+alphabet = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+Hola1 = alphabet[23] + alphabet[5] + alphabet[0] + alphabet[1] + alphabet[18] + alphabet[5] + alphabet[0] + alphabet[19] + alphabet[11] + alphabet[8] + alphabet[21] + alphabet[0] + alphabet[12] + alphabet[9] + alphabet[11] + alphabet[5] + alphabet[12] + alphabet[9] + alphabet[15] + alphabet[14]
+print(Hola1)
+Hola2 = alphabet[8] + alphabet[1] + alphabet[3] + alphabet[11] + alphabet[0] + alphabet[25] + alphabet[15] + alphabet[21] + alphabet[18] + alphabet[0] + alphabet[12] + alphabet[9] + alphabet[6] + alphabet[5]
+print(Hola2)
+Hola3 = alphabet[13] + alphabet[25] + alphabet[0] + alphabet[14] + alphabet[1] + alphabet[13] + alphabet[5] + alphabet[0] + alphabet[9] + alphabet[19] + alphabet[0] + alphabet[3] + alphabet[8] + alphabet[1] + alphabet[5] + alphabet[5] + alphabet[21] + alphabet[14]
+print(Hola3)
+
+number = ['-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+Bye1 = number[3] + number[1] + number[1] + number[5] + number[1] + number[3] + number[1] + number[8]
+print("제 생일은 %s 입니다." %Bye1)
+Bye2 = number[1] + number[2] + number[1] + number[0] + number[7] + number[8] + number[2] + number[1] + number[0] + number[4] + number[7] + number[9] +  number[3]
+print("제 전화번호는 %s 입니다." %Bye2)
+Bye3 = number[3] + number[1] + number[3] + number[3] + number[2] + number[2] + number[2] + number[4] + number[5]
+print("제 학번은 %s 입니다." %Bye3)
